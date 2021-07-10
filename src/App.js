1b0284ef7e227039dcffe7b0d86d87ef58d2074d
Aksharaa <aksharaa@Aksharaas-MBP.Home>
2021-07-10 21:37:38 +0400

    formatted code

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import logo from './my-home-town.png';
-import './App.css';
-import { 
-  useAppStore, 
-  loggedInUserNameSelector, 
-  setLoggedInUserNameSelector 
+import logo from "./my-home-town.png";
+import "./App.css";
+import {
+  useAppStore,
+  loggedInUserNameSelector,
+  setLoggedInUserNameSelector,
 } from "./hooks/useAppStore";
 
 function App() {
   const loggedInUserName = useAppStore(loggedInUserNameSelector);
   const setLoggedInUserName = useAppStore(setLoggedInUserNameSelector);
-  
+
   const changeUserName = () => {
-    const newName = "Random User " + Date.now()
-    setLoggedInUserName(newName)
-  }
-  
+    const newName = "Random User " + Date.now();
+    setLoggedInUserName(newName);
+  };
+
   return (
     <div className="tableGrid">
       <div className="logo">
-      <img src={logo} className="logoImg" alt="logo" />
+        <img src={logo} className="logoImg" alt="logo" />
       </div>
       <div className="container">
-      <div className="figure">
-        soem text
-        <hr className="line"></hr>
+        <div className="figure">
+          soem text
+          <hr className="line"></hr>
+        </div>
+        <div className="figure">
+          soem text
+          <hr className="line"></hr>
+        </div>
       </div>
-      <div className="figure">
-        soem text
-        <hr className="line"></hr>
-      </div>
-      </div>
-      </div>
+    </div>
   );
 }
 
