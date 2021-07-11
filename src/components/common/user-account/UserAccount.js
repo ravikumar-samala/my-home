78c220d53eb62a0c04e1aab0bba49c9bfbc4001f
vineeth-pappu <vineeth0394@gmail.com>
2021-07-12 01:21:50 +0530

    Update user account styles

--- src/components/common/user-account/UserAccount.js
+++ src/components/common/user-account/UserAccount.js
@@ -1,9 +1,11 @@
 import styles from "./UserAccount.module.css";
+import { BsCaretDownFill } from "react-icons/bs";
 
 const UserAccount = () => {
     return (
         <div className={styles.userAccount}>
             <span>Vineeth Viswanathan</span>
+            <BsCaretDownFill className="ml-2" />
         </div>
     )
 }
