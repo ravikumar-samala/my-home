81199ccfc816afb6b324768fd900aaea5f9c4f3a
vineeth-pappu <vineeth0394@gmail.com>
2021-07-11 23:01:19 +0530

    Add tourlist container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import TourList from './containers/tour-list/TourList';
 
 function App() {
  
   return (
     <div className="App">
-      
+		<TourList />
     </div>
   );
 }
---
+++ src/containers/tour-list/TourList.js
@@ -0,0 +1,7 @@
+import styles from "./TourList.module.css";
+
+const TourList = () => {
+    return <h1 className={styles.header}>Tourlist</h1>
+}
+
+export default TourList;
