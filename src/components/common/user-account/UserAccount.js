5ac6ef2bfdc06ee2b2fc1996d6ac25c352f2888b
vineeth-pappu <vineeth0394@gmail.com>
2021-07-12 01:12:11 +0530

    Add user account component

---
+++ src/components/common/user-account/UserAccount.js
@@ -0,0 +1,11 @@
+import styles from "./UserAccount.module.css";
+
+const UserAccount = () => {
+    return (
+        <div className={styles.userAccount}>
+            <span>Vineeth Viswanathan</span>
+        </div>
+    )
+}
+
+export default UserAccount;
