45986e44c0b519da5aa8c58045de732c247191d3
vineeth-pappu <vineeth0394@gmail.com>
2021-07-11 23:29:39 +0530

    Try bootstrap components in tourlist

--- src/containers/tour-list/TourList.js
+++ src/containers/tour-list/TourList.js
@@ -1,7 +1,16 @@
 import styles from "./TourList.module.css";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 
 const TourList = () => {
-    return <h1 className={styles.header}>Tourlist</h1>
+    return (
+        <>
+            <Container>
+                <h1 className={styles.header}>Heading</h1>
+                <Button>Test</Button>
+            </Container>
+        </>
+    )
 }
 
 export default TourList;
